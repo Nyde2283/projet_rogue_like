941beb855b4f9ef546ffd180b4e94e9faa0ddeae
Nyde2283 <edwynchev@gmail.com>
2023-03-12 17:52:27 +0100

    ➕ ADD création des rooms

--- mapping/mapping.py
+++ mapping/mapping.py
@@ -1,9 +1,35 @@
+from typing import Tuple
 from random import random, randint
 
 
+class Rectangle:
+
+    def __init__(self, x: int, y: int, width: int, height: int) -> None:
+
+        self.x = x
+        self.y = y
+        self.right = x + width -1
+        self.bottom = y + height -1
+        self.width = width
+        self.height = height
+
+    def __contains__(self, other: Tuple[int, int]) -> bool:
+
+        abscisse = self.x <= other[0] <= self.right
+        ordoonee = self.y <= other[1] <= self.bottom
+        if abscisse and ordoonee:
+            return True
+        return False
+
+class Room(Rectangle):
+
+    def __init__(self, x: int, y: int, width: int, height: int) -> None:
+
+        super().__init__(x, y, width, height)
+
 class Map_Base:
 
-    def __init__(self, x: int, y: int, width: int, height: int, minSize: int) -> None:
+    def __init__(self, x: int, y: int, width: int, height: int, minRoomSize: int, marge: int) -> None:
         """Représente une section de map, implémentée sous la forme d'un noeud d'un arbre binaire.
 
         leftChild et rightChild sont équivalents à topChild et bottomChild
@@ -22,9 +48,12 @@ class Map_Base:
         self.bottom = y + height -1
         self.width = width
         self.height = height
-        self.minSize = minSize
+        self.minRoomSize = minRoomSize
+        self.marge = marge
+        self.minSize = minRoomSize +marge
         self.leftChild = None
         self.rightChild = None
+        self.room: Room = None
     
     def split(self) -> bool:
         """Sépare self en deux Map_Base qui deviennent ses noeuds enfants si cela est possible.
@@ -53,17 +82,32 @@ class Map_Base:
         cut = randint(self.minSize, max) # split location
 
         if split_horiz:
-            self.leftChild = Map_Base(self.x, self.y, self.width, cut, self.minSize)
-            self.rightChild = Map_Base(self.x, self.y + cut, self.width, self.height - cut, self.minSize)
+            self.leftChild = Map_Base(self.x, self.y, self.width, cut, self.minRoomSize, self.marge)
+            self.rightChild = Map_Base(self.x, self.y + cut, self.width, self.height - cut, self.minRoomSize, self.marge)
         else:
-            self.leftChild = Map_Base(self.x, self.y, cut, self.height, self.minSize)
-            self.rightChild = Map_Base(self.x + cut, self.y, self.width - cut, self.height, self.minSize)
+            self.leftChild = Map_Base(self.x, self.y, cut, self.height, self.minRoomSize, self.marge)
+            self.rightChild = Map_Base(self.x + cut, self.y, self.width - cut, self.height, self.minRoomSize, self.marge)
 
         return True
 
+    def create_rooms(self):
+
+        if self.leftChild != None:
+            self.leftChild.create_rooms()
+            self.rightChild.create_rooms()
+        else:
+            # la taille d'une room varie de minRoomSize x minRoomSize à la taille de la Map_base -2
+            roomWidth = randint(self.minRoomSize, self.width -2)
+            roomHeight = randint(self.minRoomSize, self.height -2)
+            # place la map de façon à ce qu'elle ne colle pas les bords de la Map_Base
+            roomX = randint(1, self.width - roomWidth)
+            roomY = randint(1, self.height - roomHeight)
+
+            self.room = Room(self.x + roomX, self.y + roomY, roomWidth, roomHeight)
+
 class Map:
 
-    def __init__(self, width: int, height: int, minSize: int, maxSize: int) -> None:
+    def __init__(self, width: int, height: int, minRoomSize: int = 4, maxRoomSize: int = 10, marge: int = 4) -> None:
         """Représente la map d'un niveau.
 
         Args:
@@ -73,13 +117,15 @@ class Map:
             maxSize (int): taille (largeur ou hauteur) maximale d'une section de la map
         """
 
-        assert maxSize > minSize and width > maxSize and height > maxSize
         self.width = width
         self.height = height
-        self.minSize = minSize
-        self.maxSize = maxSize
+        self.minRoomSize = minRoomSize
+        self.maxRoomSize = maxRoomSize
+        self.minSize = minRoomSize +marge
+        self.maxSize = maxRoomSize +2
+        assert self.maxSize > self.minSize and width > self.maxSize and height > self.maxSize
 
-        self.root = Map_Base(0, 0, width, height, self.minSize)
+        self.root = Map_Base(0, 0, width, height, self.minRoomSize, marge)
         self.maps_list = [self.root]
 
         did_split = True
@@ -93,3 +139,4 @@ class Map:
                         self.maps_list.append(map.leftChild)
                         self.maps_list.append(map.rightChild)
                         did_split = True
+        self.root.create_rooms()
